6df44a8bfe1eea62af5b5167bfce826c42649bfc
Kota <maedakoutaa@gmail.com>
2022-12-19 08:37:00 +0900

    Initial Commit

---
+++ MiniApp116-OpeningSlideView/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MiniApp116-OpeningSlideView
+//
+//  Created by 前田航汰 on 2022/12/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MiniApp116-OpeningSlideView/MiniApp116_OpeningSlideViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniApp116_OpeningSlideViewApp.swift
+//  MiniApp116-OpeningSlideView
+//
+//  Created by 前田航汰 on 2022/12/19.
+//
+
+import SwiftUI
+
+@main
+struct MiniApp116_OpeningSlideViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
